01ad0d3fd8e1d74688193ca00373aa2487d8d6c1
borisvicena <boris.vicena@gmail.com>
2020-05-30 08:47:37 -0700

    pridane ovladanie kamery

---
+++ Assets/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,81 @@
+﻿using Gameplay;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> players;
+
+    public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (players.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (players.Count == 1)
+        {
+            return players[0].position;
+        }
+
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++) 
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+
+        return bounds.center;
+    }
+
+}
