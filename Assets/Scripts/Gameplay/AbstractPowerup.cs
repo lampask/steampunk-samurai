5a03dd7527dd19b032abf8c31f6be3075a268f7b
Lampask <adamgramblicka@hotmail.com>
2020-05-30 16:04:29 +0200

    New structure (unstable)

---
+++ Assets/Scripts/Gameplay/AbstractPowerup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public abstract class AbstractPowerup : MonoBehaviour
+    {
+        public abstract Vector2 position { get; protected set; }
+        public abstract Sprite texture {get; protected set;}
+        
+        protected GameObject obj { get; private set; }
+
+        public abstract void OnPickup(Player target);
+
+        protected virtual void Awake()
+        {
+            obj = Instantiate(new GameObject(), position, Quaternion.identity);
+            ((SpriteRenderer) obj.AddComponent(typeof(SpriteRenderer))).sprite = texture;
+        }
+
+        protected void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out Player p))
+            {
+                OnPickup(p);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Models
+{
+    public class PlayerModel
+    {
+        public int id { get; set; }
+    }
+}
