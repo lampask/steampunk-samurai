d3de96d63f822e96590bd665da6fb03edeb6819a
Lampask <adamgramblicka@hotmail.com>
2020-06-30 22:11:31 +0200

    basic arena loading

--- Assets/Scripts/Management/LocalGameManager.cs
+++ Assets/Scripts/Management/LocalGameManager.cs
@@ -1,7 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Behaviours;
+using UnityEngine;
 using UnityEngine.Events;
 using Definitions;
+using GameEnvironment;
 using Models;
+using Utilities;
+using Random = System.Random;
 
 namespace Management
 {
@@ -10,41 +17,121 @@ namespace Management
     {
         public static LocalGameManager instance;
         public static LoadDefinitions definitions { get; protected set; }
-        public static LoadModels models;
+        public static LoadModels models { get; protected set; }
 
+        public Transform barObject;
+
+        private List<Tuple<PlayerModel, PlayerDefinition>> playerComponents;
+        private Tuple<ArenaModel, ArenaDefinition> arenaComponents;
+        
+        // IN GAME VARS
+
+        public GameObject arena;
+        public List<PlayerBehaviour> players;
+
+        public Dictionary<int, Tuple<PlayerModel, PlayerDefinition>> playerData;
+        
+        // GAME EVENTS
+        
         public PlayerEvent onPlayerHealthChangeEvent;
         public PlayerEvent onPlayerEnergyChangeEvent;
-        
+        public UnityEvent gameStart;
+        public UnityEvent gameOver;
+        public PlayerEvent roundFinished;
+        public UnityEvent powerUpSpawned;
+
         private void Awake() {
             if (!instance)
                 instance = this;
             else
                 Destroy(this);
             
-            if (onPlayerHealthChangeEvent != null) onPlayerHealthChangeEvent = new PlayerEvent();
-            if (onPlayerEnergyChangeEvent != null) onPlayerEnergyChangeEvent = new PlayerEvent();
+            // Initialize events
+            if (onPlayerHealthChangeEvent == null) onPlayerHealthChangeEvent = new PlayerEvent();
+            if (onPlayerEnergyChangeEvent == null) onPlayerEnergyChangeEvent = new PlayerEvent();
+
+            playerComponents = MenuManager.instance.selections.Where(s => s.confirmed).ToList().Select(s => s.ConvertToDefiningComponents()).ToList();
+            //arenaComponents = MenuManager.instance.arena.ConvertToDefiningComponents();
+            
+            // Initialize easily accessible bridge
+            playerData = playerComponents.ToDictionary(p => p.Item1.id);
+
+
+            // We're freezing bois ...
+            Time.timeScale = 0;
         }
         
-        void Start()
+        private void Start()
         {
-            LoadModels();
+            // Load external components
+            LoadComponents();
+            
             // Load Arena
+            arena = new GameObject("ArenaHolder");
             
+            /*
+             * 0 - Backgrounds
+             * 1 - Buildings
+             * 2 - Effects
+             */
+            // TODO: move asset loading to imports 
             
-            // Load Players
-            foreach (var def in definitions.players)
+            // 0 
+            var bgs = new GameObject("Backgrounds");
+            foreach (var b in arenaComponents.Item2.backgrounds)
             {
-                
+                var a = (GameObject) Instantiate(Resources.Load($"Prefabs/Backgrounds/{b.asset}"), Vector3.zero,
+                    Quaternion.identity);
+                a.transform.SetParent(bgs.transform);
+                a.GetComponent<SpriteRenderer>().sortingOrder = (int) b.parallaxPosition;
             }
+            
+            // 1
+            var buildings = new GameObject("Buildings");
+            foreach (var b in arenaComponents.Item2.buildings)
+            {
+                var a = (GameObject) Instantiate(Resources.Load($"Prefabs/Buildings/{b.asset}"), b.position,
+                    Quaternion.identity);
+                a.transform.SetParent(bgs.transform);
+                a.GetComponent<SpriteRenderer>().sortingOrder = b.layer;
+            }
+            
+            // 2 (idk will be used in future)
+            var effects = new GameObject("Effects");
+            arenaComponents.Item2.effects.ForEach(e => Instantiate(new GameObject(), e.position, Quaternion.identity).transform.SetParent(effects.transform));
+            
+            // Create hierarchy
+
+            bgs.transform.SetParent(arena.transform);
+            buildings.transform.SetParent(arena.transform);
+            effects.transform.SetParent(arena.transform);
+            
+            
+            // Load Players into generated arena
+            
+            // Shuffle spawn points
+            var rnd = new Random();
+            var spawnPoints = arenaComponents.Item2.spawnLocations[MenuManager.instance.confirmed]
+                .Select(x => new {value = x, order = rnd.Next()})
+                .OrderBy(x => x.order).Select(x => x.value).ToList();
+            
+            playerComponents.ForEach(c =>
+            {
+                players.Add(Instantiate(Imports.PlayerObject,
+                    spawnPoints[c.Item1.id].position,
+                    Quaternion.identity).GetComponent<PlayerBehaviour>());
+                
+                // Generate bars ==> PlayerInfo Objects
+                ((GameObject) Instantiate(Resources.Load("Prefabs/Bar"))).transform.SetParent(barObject);
+            });
+            
         }
 
-        void LoadModels()
+        void LoadComponents()
         {
+            // Load from external sources
             definitions = new LoadDefinitions();
             models = new LoadModels();
         }
-
-        
-        
     }
 }
